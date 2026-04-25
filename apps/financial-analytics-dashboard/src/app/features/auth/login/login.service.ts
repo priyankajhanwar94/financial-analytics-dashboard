e668b453a3ba09c799e707c6249da33b056a2f0d
Priyanka <priyanka@Priyankas-MacBook-Air.local>
2026-04-25 22:13:14 +0530

    fix:updated api base urlin prod env

--- apps/financial-analytics-dashboard/src/app/features/auth/login/login.service.ts
+++ apps/financial-analytics-dashboard/src/app/features/auth/login/login.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Login, LoginData } from './login.model';
-import { environment } from 'apps/financial-analytics-dashboard/src/environments/environment';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({providedIn: 'root'})
 export class LoginService {
