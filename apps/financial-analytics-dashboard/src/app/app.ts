ec9936b85df088d1bd48f5b044815580e6d191fb
Priyanka <priyanka@Priyankas-MacBook-Air.local>
2026-04-27 09:50:41 +0530

    fix:global loader condition moved out of loader component

--- apps/financial-analytics-dashboard/src/app/app.ts
+++ apps/financial-analytics-dashboard/src/app/app.ts
@@ -1,16 +1,25 @@
-import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Loader } from './shared/components/loader/loader';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+import { LoaderService } from './core/services/loader.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.html',
-  imports: [RouterModule, Loader],
+  imports: [RouterModule, Loader, AsyncPipe],
   // providers:[{provide: HTTP_INTERCEPTORS, useClass : AuthInterceptor}]
 })
+
 export class App {
+  isLoading: Observable<boolean>;
+  private loaderService = inject(LoaderService);
+  constructor() {
+    this.isLoading = this.loaderService.isLoading$;
+  }
   message = '';
   data={};
 }
+
--- apps/financial-analytics-dashboard/src/app/shared/components/loader/loader.ts
+++ apps/financial-analytics-dashboard/src/app/shared/components/loader/loader.ts
@@ -1,18 +1,8 @@
 import { Component, inject } from '@angular/core';
-import { Observable } from 'rxjs/internal/Observable';
-import { LoaderService } from '../../../core/services/loader.service';
-import { AsyncPipe } from '@angular/common';
 @Component({
   selector: 'app-loader',
-  imports: [AsyncPipe],
+  imports: [],
   templateUrl: './loader.html',
   styleUrl: './loader.css',
 })
-export class Loader {
-  isLoading: Observable<boolean>;
-  private loaderService = inject(LoaderService);
-
-  constructor() {
-    this.isLoading = this.loaderService.isLoading$;
-  }
-}
+export class Loader {}
