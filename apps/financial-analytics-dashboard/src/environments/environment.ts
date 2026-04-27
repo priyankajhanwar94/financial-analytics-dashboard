1c49754d29dc51bcf3711940d4cef12964881ead
Priyanka <priyanka@Priyankas-MacBook-Air.local>
2026-04-27 22:22:35 +0530

    fix:backend URL

--- apps/financial-analytics-dashboard/src/environments/environment.ts
+++ apps/financial-analytics-dashboard/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5000'
-  // apiUrl: 'https://financial-analytics-dashboard-zea8.onrender.com'
+  apiUrl: 'https://financial-analytics-dashboard-zea8.onrender.com'
 };
